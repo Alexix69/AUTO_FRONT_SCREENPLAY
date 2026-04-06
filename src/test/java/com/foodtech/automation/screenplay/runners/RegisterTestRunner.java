4823edff3a98b1f5fd71c745100099d0a48df7e4
Alexis Yepez <alexixyepex@gmail.com>
2026-04-06 15:34:14 -0500

    feat: Update test configuration for JUnit 5 and Cucumber integration

--- src/test/java/com/foodtech/automation/screenplay/runners/RegisterTestRunner.java
+++ src/test/java/com/foodtech/automation/screenplay/runners/RegisterTestRunner.java
@@ -1,14 +1,17 @@
 package com.foodtech.automation.screenplay.runners;
 
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
-import io.cucumber.junit.CucumberOptions;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(
-	features = "classpath:features/register",
-	glue = "com.foodtech.automation.screenplay.stepdefinitions",
-	plugin = {"pretty"}
-)
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features/register")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.foodtech.automation.screenplay.stepdefinitions")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "net.serenitybdd.cucumber.core.plugin.SerenityReporterParallel,pretty")
 public class RegisterTestRunner {
 }
